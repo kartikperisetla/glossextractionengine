62d5dba155f5fe744d75872e97eff7b705c9ea66
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-08 00:37:56 -0500

    adding arg_parser

---
+++ lib/utils/arg_parser.py
@@ -0,0 +1,22 @@
+__author__ = 'kartik'
+
+class ArgParser(object):
+    def __init__(self):
+        print "ArgParser:init"
+        self.args = {}
+
+    def parse(self, lst):
+        for index, item in enumerate(lst):
+            if "-" in item:
+                _act_param = item.replace("-","")
+                if len(lst)<=index+1:
+                    print "ArgParser:Error:No value provided for param: ",_act_param
+                    exit()
+
+                self.args[_act_param]= lst[index+1]
+
+    def get_string(self):
+        str = ""
+        for key,val in self.args.iteritems():
+            str = str+" -"+key+" "+val
+        return str
