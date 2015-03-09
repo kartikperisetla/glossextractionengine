804a3cc22e9229278a4f5f2d4decddf2fabcdb68
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-08 21:39:03 -0400

    updating malt parsed feature extraction flow mapper to read params provided by user and instantiate appropriate feature extractor

--- lib/mapreduce/malt_parsed_feature_extraction_flow_mapper.py
+++ lib/mapreduce/malt_parsed_feature_extraction_flow_mapper.py
@@ -10,10 +10,10 @@ from lib.feature_extractor.malt_parsed_pos_context_sequence_feature_extractor im
 
 # mapper side - class that acts as proxy for feature extractor for malt parsed sentences
 class MaltParsedFeatureExtractionFlowMapper:
-    def __init__(self):
+    def __init__(self, context_window_size=4, prime_feature_length=4, add_prime_feature = False):
         # get hold of suitable feature extractor
         # k_param: pass 4 tokens as context window length:
-        self.feature_extractor = MaltParsedPOSContextSequenceFeatureExtractor(k_param=4)
+        self.feature_extractor = MaltParsedPOSContextSequenceFeatureExtractor(k_param=context_window_size,prime_feature_length=prime_feature_length, add_prime_feature=add_prime_feature)
 
     def process(self, line):
 
@@ -30,7 +30,16 @@ class MaltParsedFeatureExtractionFlowMapper:
 
 
 if __name__ == '__main__':
-    _instance = MaltParsedFeatureExtractionFlowMapper()
+    if len(sys.argv)<2:
+        print "Too few arguments to instantiate MaltParsedFeatureExtractionFlowMapper"
+        exit()
+    _args = sys.argv[1]
+    params = _args.split("#")
+    _context_window_size = int(params[0])
+    _prime_feature_length = int(params[1])
+    _add_prime_feature = bool(params[2])
+
+    _instance = MaltParsedFeatureExtractionFlowMapper(context_window_size=_context_window_size, prime_feature_length=_prime_feature_length, add_prime_feature=_add_prime_feature)
 
     # a single line is <category> '<instance_name> | <instance>
     for line in sys.stdin:
