7eac75ad6b48d6daf4f6927c8e63da1fa4ff0eeb
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-11 19:45:51 -0400

    adding method to show most informative features in the trained model

--- lib/modeler/base_modeler.py
+++ lib/modeler/base_modeler.py
@@ -66,6 +66,19 @@ class BaseModeler(object):
         pickle.dump(self.entity, _pobj)
         _pobj.close()
 
+        self.save_informative_features(_location)
+
+
+    # method to save the most informative features of the trained model
+    def save_informative_features(self, location):
+        # if modeler has valid classifier
+        if not self.entity is None:
+            _result = self.entity.most_informative_features()
+            _path = location + "/" + self.model_saved_name+"_informative_features"
+            _fobj = open(_path,"wb")
+            pickle.dump(_result, _fobj)
+            _fobj.close()
+
     # method to load the model from pickle file
     # it updates the self.entity attribute once it loads the model from pickle file
     def load_model(self,**kwargs):
