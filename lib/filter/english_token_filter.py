c15700ae8283f7215ac05a28a3021b8225659bbd
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-11 19:10:04 -0400

    adding filter that returns true if sentence or token is english word

--- lib/filter/english_token_filter.py
+++ lib/filter/english_token_filter.py
@@ -1,1 +1,14 @@
 __author__ = 'kartik'
+
+from lib.filter.base_filter import BaseFilter
+import re
+import string
+
+# class that filters sentences based on sentence length
+class EnglishTokenFilter(BaseFilter):
+    # method that returns True if sentence contains only english tokens
+    def filter(self, sentence):
+        if set('[~!@#$%^&*()_+{}":;\']+$').intersection(sentence):
+            return False
+        else:
+            return True
