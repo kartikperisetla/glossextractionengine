a971746876f4cd05a7770826615022997fa89de3
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-10 23:12:18 -0400

    catching the exception and printing message

--- lib/mapreduce/feature_extraction_flow_mapper.py
+++ lib/mapreduce/feature_extraction_flow_mapper.py
@@ -12,6 +12,7 @@ class FeatureExtractionFlowMapper:
     def __init__(self, context_window_size=4, prime_feature_length=4, add_prime_feature = False):
         # get hold of suitable feature extractor
         # k_param: pass 4 tokens as context window length:
+
         self.feature_extractor = POSContextSequenceFeatureExtractor(k_param=context_window_size,prime_feature_length=prime_feature_length, add_prime_feature=add_prime_feature)
 
     def process(self, line):
@@ -19,7 +20,8 @@ class FeatureExtractionFlowMapper:
             feature_dict,category,word = self.feature_extractor.extract_features(line)
             if not feature_dict is None:
                 print word,"\t",category,"\t",feature_dict
-        except:
+        except Exception as ex:
+            print >>sys.stderr,ex.message
             pass
 
 if __name__ == '__main__':
@@ -28,9 +30,9 @@ if __name__ == '__main__':
         exit()
     _args = sys.argv[1]
     params = _args.split("#")
-    _context_window_size = int(params[0])
-    _prime_feature_length = int(params[1])
-    _add_prime_feature = bool(params[2])
+    _context_window_size = int(params[0].strip())
+    _prime_feature_length = int(params[1].strip())
+    _add_prime_feature = bool(params[2].strip())
     _instance = FeatureExtractionFlowMapper(context_window_size=_context_window_size, prime_feature_length=_prime_feature_length, add_prime_feature=_add_prime_feature)
 
     # a single line is <category> '<instance_name> | <instance>
