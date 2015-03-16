ef3e7c8dee325d66726feb3809a15d7d5a41ee47
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-16 18:29:22 -0400

    updating to take custom threshold param from user via commandline

--- lib/mapreduce/cross_validation_feature_extraction_flow_mapper.py
+++ lib/mapreduce/cross_validation_feature_extraction_flow_mapper.py
@@ -8,7 +8,7 @@ sys.path.insert(0, 'glossextractionengine.mod')
 from lib.feature_extractor.pos_context_sequence_feature_extractor import POSContextSequenceFeatureExtractor
 
 # mapper side - class that acts as proxy for feature extractor
-class FeatureExtractionFlowMapper:
+class CrossValidationFeatureExtractionFlowMapper:
     def __init__(self, context_window_size=4, prime_feature_length=4, add_prime_feature_val = False):
         # get hold of suitable feature extractor
         # k_param: pass 4 tokens as context window length:
@@ -28,13 +28,13 @@ if __name__ == '__main__':
     if len(sys.argv)<2:
         print "Too few arguments to instantiate FeatureExtractionFlowMapper"
         exit()
-    _args = sys.argv[1]
+    _args = sys.argv[2]
     params = _args.split("#")
     _context_window_size = int(params[0].strip())
     _prime_feature_length = int(params[1].strip())
     _add_prime_feature = ast.literal_eval(params[2].strip())
     print>>sys.stderr," FeatureExtractionFlowMapper:_add_prime_feature: ",_add_prime_feature
-    _instance = FeatureExtractionFlowMapper(context_window_size=_context_window_size, prime_feature_length=_prime_feature_length, add_prime_feature_val=_add_prime_feature)
+    _instance = CrossValidationFeatureExtractionFlowMapper(context_window_size=_context_window_size, prime_feature_length=_prime_feature_length, add_prime_feature_val=_add_prime_feature)
 
 
     # a single line is <category> '<instance_name> | <instance>
--- lib/mapreduce/cross_validation_feature_extraction_flow_reducer.py
+++ lib/mapreduce/cross_validation_feature_extraction_flow_reducer.py
@@ -8,13 +8,14 @@ sys.path.insert(0, 'glossextractionengine.mod')
 from lib.modeler.pos_context_sequence_modeler import POSContextSequenceModeler
 from ast import literal_eval
 
-class FeatureExtractionFlowClassifierReducer:
-    def __init__(self, saved_model_name):
+class CrossValidationFeatureExtractionFlowReducer:
+    def __init__(self, saved_model_name, threshold_value = 85):
         pass
         self._instance = POSContextSequenceModeler()
         coll = saved_model_name.split("/")
         saved_model_name = coll[-1:][0]
         self._instance.load_model(name = saved_model_name)
+        self.threshold = threshold_value
 
     def process(self, line):
         try:
@@ -25,10 +26,20 @@ class FeatureExtractionFlowClassifierReducer:
                 feature_dict = _collection[2]
 
                 _inst_feature_dict = literal_eval(feature_dict)
-                result = self._instance.classify(_inst_feature_dict)
+                prob_dist = self._instance.entity.prob_classify(_inst_feature_dict)
+
+                pos_prob = prob_dist.prob('1')
+                neg_prob = prob_dist.prob('-1')
+                print>>sys.stderr, " pos_prob:",pos_prob
+                print>>sys.stderr, " neg_prob:",neg_prob
+
+                if float(pos_prob)*100 >= float(self.threshold):
+                    result = "1"
+                else:
+                    result = "-1"
 
                 flag = "NO-MATCH"
-                if str(category) == str(result):
+                if str(category.strip()) == str(result.strip()):
                     flag = "MATCH"
 
                 feature_category_tuple = (word,result,category,flag, feature_dict, line)
@@ -38,7 +49,9 @@ class FeatureExtractionFlowClassifierReducer:
 
 
 if __name__ == '__main__':
-    _instance = FeatureExtractionFlowClassifierReducer()
+    _model_file = sys.argv[1]
+    _threshold_value = sys.argv[2]
+    _instance = CrossValidationFeatureExtractionFlowReducer(_model_file, _threshold_value)
 
     # a single line is <category> '<instance_name> | <instance>
     for line in sys.stdin:
