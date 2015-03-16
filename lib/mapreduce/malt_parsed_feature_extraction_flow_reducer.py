e8117aee35676b109b00e76b1b527df10337ad80
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-16 18:30:24 -0400

    updating to take custom threshold param from user via commandline -cl_reducer_params

--- lib/mapreduce/malt_parsed_feature_extraction_flow_reducer.py
+++ lib/mapreduce/malt_parsed_feature_extraction_flow_reducer.py
@@ -13,12 +13,13 @@ from ast import literal_eval
 
 
 class MaltParsedFeatureExtractionFlowReducer:
-    def __init__(self, saved_model_name):
+    def __init__(self, saved_model_name, threshold_value = 85):
         pass
         self._instance = POSContextSequenceModeler()
         coll = saved_model_name.split("/")
         saved_model_name = coll[-1:][0]
         self._instance.load_model(name = saved_model_name)
+        self.threshold = threshold_value
 
     def process(self, line):
         if line.strip()!="":
@@ -29,7 +30,21 @@ class MaltParsedFeatureExtractionFlowReducer:
                 feature_dict = _collection[1]
                 line = _collection[2]
                 _inst_feature_dict = literal_eval(feature_dict)
-                result = self._instance.classify(_inst_feature_dict)
+                # result = self._instance.classify(_inst_feature_dict)
+
+                # prob classify approach
+                prob_dist = self._instance.entity.prob_classify(_inst_feature_dict)
+
+                pos_prob = prob_dist.prob('1')
+                neg_prob = prob_dist.prob('-1')
+                print>>sys.stderr, " pos_prob:",pos_prob
+                print>>sys.stderr, " neg_prob:",neg_prob
+
+                if float(pos_prob)*100 >= float(self.threshold):
+                    result = "1"
+                else:
+                    result = "-1"
+
 
                 # output only positive instances
                 if not "-" in result:
@@ -45,7 +60,8 @@ if __name__ == '__main__':
         print "MaltParsedFeatureExtractionFlowReducer: need model as parameter name"
     else:
         _model_file = sys.argv[1]
-        _instance = MaltParsedFeatureExtractionFlowReducer(_model_file)
+        _threshold_value = sys.argv[2]
+        _instance = MaltParsedFeatureExtractionFlowReducer(_model_file, _threshold_value)
 
         # a single line is <word>,"\t",<feature_dict>,"\t",<line>
         for line in sys.stdin:
