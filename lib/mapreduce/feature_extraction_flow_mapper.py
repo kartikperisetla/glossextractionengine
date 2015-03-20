06031721f3bc9bc572a6721543029166949fea4a
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-19 20:30:47 -0400

    updated feature extraction flow mapper to inherit BaseFlowComponent; invoke get_features to get features from all registered feature extractors

--- lib/mapreduce/feature_extraction_flow_mapper.py
+++ lib/mapreduce/feature_extraction_flow_mapper.py
@@ -6,22 +6,34 @@ import cStringIO
 sys.path.insert(0, 'glossextractionengine.mod')
 
 from lib.feature_extractor.pos_context_sequence_feature_extractor import POSContextSequenceFeatureExtractor
+from lib.mapreduce.base_flow_component import BaseFlowComponent
 
 # mapper side - class that acts as proxy for feature extractor
-class FeatureExtractionFlowMapper:
+class FeatureExtractionFlowMapper(BaseFlowComponent):
     def __init__(self, context_window_size=4, prime_feature_length=4, add_prime_feature_val = False):
+        super(self.__class__, self).__init__()
+
         # get hold of suitable feature extractor
         # k_param: pass 4 tokens as context window length:
 
-        self.feature_extractor = POSContextSequenceFeatureExtractor(k_param=context_window_size,prime_feature_length=prime_feature_length, add_prime_feature=add_prime_feature_val)
+        # instantiate a pos context sequence feature extractor
+        pos_context_seq_feature_extractor = POSContextSequenceFeatureExtractor(k_param=context_window_size,prime_feature_length=prime_feature_length, add_prime_feature=add_prime_feature_val)
+        # register the feature extractor to this flow component
+        self.register_feature_extractor(pos_context_seq_feature_extractor)
 
+        # instantiate any other feature extractor and register it with flow components to start using it in the flow
+
+    # param: string of format- <category> '<instance_name> | <instance>
     def process(self, line):
         try:
-            feature_dict,category,word = self.feature_extractor.extract_features(line)
-            if not feature_dict is None:
-                print word,"\t",category,"\t",feature_dict
+            # invokes feature extraction on all registered feature extractors
+            result_tuple_collection = self.get_features(line)
+            for tuple_instance in result_tuple_collection:
+                feature_dict,category,word,sentence = tuple_instance
+                if not feature_dict is None:
+                    print word,"\t",category,"\t",feature_dict
         except Exception as ex:
-            print >>sys.stderr,ex.message
+            print >>sys.stderr,ex
             pass
 
 if __name__ == '__main__':
@@ -35,7 +47,6 @@ if __name__ == '__main__':
     _add_prime_feature = ast.literal_eval(params[2].strip())
     print>>sys.stderr," FeatureExtractionFlowMapper:_add_prime_feature: ",_add_prime_feature
     _instance = FeatureExtractionFlowMapper(context_window_size=_context_window_size, prime_feature_length=_prime_feature_length, add_prime_feature_val=_add_prime_feature)
-
 
     # a single line is <category> '<instance_name> | <instance>
     for line in sys.stdin:
