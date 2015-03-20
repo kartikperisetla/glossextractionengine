4e93ddfe1f0c9c86571cd3c122ae34ac7084c2d6
Kartik Perisetla <kartik.peri@gmail.com>
2015-03-19 21:40:46 -0400

    updated to handle result when it is list of tuple or single tuple

--- lib/mapreduce/base_flow_component.py
+++ lib/mapreduce/base_flow_component.py
@@ -31,11 +31,15 @@ class BaseFlowComponent(object):
 
         for fe_object in self.feature_extractor_collection:
             # feat_dict,cat,wrd = fe_object.extract_features(instance)
+            print>>sys.stderr," using FE currently:",fe_object
             _tuple_instance_collection = fe_object.extract_features(instance)
-
-            for _tuple_instance in _tuple_instance_collection:
-                print "BaseFlowComponent got:", _tuple_instance
-                self.update_tuple(_tuple_instance)
+            # print>>sys.stderr,"BaseFlowComponent got _tuple_instance_collection:",_tuple_instance_collection
+            if isinstance(_tuple_instance_collection,list):
+                for _tuple_instance in _tuple_instance_collection:
+                    # print>>sys.stderr,"BaseFlowComponent got _tuple_instance:", _tuple_instance
+                    self.update_tuple(_tuple_instance)
+            else:
+                self.update_tuple(_tuple_instance_collection)
 
         # return the list of tuples
         result = list(self.tuple_collection.values())
@@ -45,6 +49,7 @@ class BaseFlowComponent(object):
     # method to update the required properties
     def update_tuple(self, tuple_instance):
         if not tuple_instance is None:
+            print>>sys.stderr," update_tuple:got tuple_instance:",tuple_instance
             new_feat_dict,new_cat,new_wrd,new_sentence = tuple_instance
             # if valid feature dictionary
             if not new_feat_dict is None:
@@ -54,7 +59,9 @@ class BaseFlowComponent(object):
                     existing_fe_dict, existing_cat, exiting_wrd, existing_sentence = existing_tuple
                     # update the feature dictionary within the tuple with feature dictionary from new given tuple provided as param
                     existing_fe_dict.update(new_feat_dict)
-
+                    # update the dict as well
+                    self.tuple_collection[new_wrd] = (existing_fe_dict, existing_cat, exiting_wrd, existing_sentence)
+                    print>>sys.stderr," updated tuple became:",self.tuple_collection[new_wrd]
                     if existing_cat is None:
                         existing_cat = new_cat
                 else:
